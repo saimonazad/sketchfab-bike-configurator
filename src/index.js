9a250418aea156af7cb9b8c08fc3751bf2b6bdd5
Alauddin Al Azad <saimon.azad@hotmail.com>
2022-10-24 17:32:41 +0600

    updated

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-if (process.env.NODE_ENV === "production") {
-  console.log = () => {};
-  console.error = () => {};
-  console.debug = () => {};
-}
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
